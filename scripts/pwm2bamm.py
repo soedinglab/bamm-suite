f1b8cf2ac5b08e5e3322554ae53e8c908800f8b5
mwess <maxwess@googlemail.com>
2018-01-24 17:01:39 +0100

    fixed root dir bug and added output directory option

--- scripts/pwm2bamm.py
+++ scripts/pwm2bamm.py
@@ -14,19 +14,25 @@ import numpy as np
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('meme_file')
+    parser.add_argument('-o', default=None)
     return parser
 
 def main():
     parser = create_parser()
     args = parser.parse_args()
     ipath = args.meme_file
-    dir = os.path.dirname(ipath)
+    if args.o is None:
+        dir = os.path.dirname(ipath)
+    else:
+        dir = args.o
+        if not os.path.exists(dir):
+            os.makedirs(dir)
     basename = os.path.splitext(os.path.basename(ipath))[0]
     motifset = parse_meme(ipath)
     models = motifset['models']
     for num in range(len(models)):
-        filepath_v = dir + '/' + basename + "_motif_" + str(num+1) + ".ihbcp"
-        filepath_p = dir + '/' + basename + "_motif_" + str(num+1) + ".ihbp"
+        filepath_v = os.path.join(dir, basename + "_motif_" + str(num+1) + ".ihbcp")
+        filepath_p = os.path.join(dir, basename + "_motif_" + str(num+1) + ".ihbp")
         write_bamm(models[num]['pwm'], filepath_v )
         write_bamm(models[num]['pwm'], filepath_p )
 
